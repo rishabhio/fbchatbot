d50e8d6e68c5f8d5142f9e9150b7fec764758397
garianandroid <garianandroidgames@gmail.com>
2016-08-12 03:15:06 +0530

    added express app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var http = require('http');
 
-	var http = require('http');
-	http.createServer(function(request,response){
-		response.writeHead(200,{"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
-	}).listen(80);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 	
